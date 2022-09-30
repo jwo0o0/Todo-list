0f42cfee64f5a2fc0d0dd3497646d55f2cd06f52
jwo0o0 <jwo0o0358@gmail.com>
2022-09-30 22:22:39 +0900

    :lipstick: style: Header component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,32 @@
+import styled from "styled-components"
 
-const Header = () => {
+export const TitleContainer = styled.div`
+    width: 100%;
+    height: 150px;
+    border: 1px solid black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
 
+export const Title = styled.div`
+    font-family: 'Nanum Gothic', sans-serif;
+    font-weight: 700;
+    font-size: 40px;
+    color: #434343;
+
+    width: 250px;
+    border-bottom: 2px solid #B2C3FF;
+    text-align: center;
+
+`
+
+
+export const Header = () => {
+    return (
+        <TitleContainer>
+            <Title>Todo List</Title>
+        </TitleContainer>
+    )
 }
 
-export { Header }
