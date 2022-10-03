3f5ff370d1b6f723326294965ec9caa733a31a8a
jwo0o0 <jwo0o0358@gmail.com>
2022-10-03 18:14:34 +0900

    :lipstick: style: add Dateinfo background image

--- src/FeatureComponents/DateInfo.js
+++ src/FeatureComponents/DateInfo.js
@@ -1,22 +1,29 @@
 import styled from "styled-components";
+import date_background_img from "../images/Dateinfo-background.svg";
 
 export const DateContainer = styled.div`
     
 `;
 
 export const TodayDate = styled.div`
-    width: 190px;
-    height: 50px;
-    border: 1px solid #434343;
-    color: #434343;
+    width: 194px;
+    position: relative;
     text-align: center;
     margin: 0 90px;
-    border-radius: 30px;
-    background-color: #D6DEFD;
 
-    display: flex;
-    justify-content: center;
-    align-items: center;
+    > img {
+        width: 100%;
+        vertical-align: middle;
+    }
+    > a {
+        position: absolute;
+        top: 40%;
+        left: 50%;
+        text-align: center;
+        transform: translate(-50%, -50%);
+        background-color: transparent;
+        color: #434343;
+    }
 `
 const now = new Date();
 const today = `${now.getFullYear()}. ${String(now.getMonth() + 1).padStart(2, '0')}. ${String(now.getDate()).padStart(2, '0')}`;
@@ -24,7 +31,10 @@ const today = `${now.getFullYear()}. ${String(now.getMonth() + 1).padStart(2, '0
 export const DateInfo = () => {
     return (
         <DateContainer>
-            <TodayDate>{today}</TodayDate>
+            <TodayDate>
+                <img src={date_background_img} />
+                <a>{today}</a>
+            </TodayDate>
         </DateContainer>
     )    
 }
