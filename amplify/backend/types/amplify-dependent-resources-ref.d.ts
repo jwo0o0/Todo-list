f82fff5871f9931dfc0530705708f89ad277efb4
jwo0o0 <jwo0o0358@gmail.com>
2022-10-22 22:09:07 +0900

    :rocket: reset aws-amplify

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,6 +1,21 @@
 export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "todolist2b4318f5": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "todoamplifyapp": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    },
     "auth": {
-        "amplifyappeddb8a2b": {
+        "todolist0ab50ab4": {
             "IdentityPoolId": "string",
             "IdentityPoolName": "string",
             "UserPoolId": "string",
--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ Amplify.configure(awsconfig);
 
 function App() {
   return (
-      <div className="App">
-        <Header />
-        <Main />
-      </div>
+    <div className="App">
+      <Header />
+      <Main />
+    </div>
   );
 }
 
