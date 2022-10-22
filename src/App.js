09f8fc39c1aeb4a51259839d5315227d04fb6e15
jwo0o0 <jwo0o0358@gmail.com>
2022-10-22 18:48:20 +0900

    :rocket: build: AWS Amplify setting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 import { Header } from './Components/Header';
 import { Main } from './Components/Main';
 
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
+
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Main />
-    </div>
+      <div className="App">
+        <Header />
+        <Main />
+      </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App)
