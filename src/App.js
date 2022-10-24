9a308259e7c45a7260ce588bf32186f02f21d69c
jwo0o0 <jwo0o0358@gmail.com>
2022-10-24 23:38:57 +0900

    backend api 테스트

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import { Amplify } from 'aws-amplify';
+import { Amplify, API } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import { Header } from './Components/Header';
 import { Main } from './Components/Main';
 
 import awsconfig from './aws-exports';
-Amplify.configure(awsconfig);
+Amplify.configure({
+  API: {
+    endpoints: [
+      {
+        name: "todolistServerApi",
+        endpoint: "https://19xppq3qld.execute-api.ap-northeast-2.amazonaws.com/dev/api/todos/"
+      }
+    ]
+  }
+});
 
 function App() {
   return (
